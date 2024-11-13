486761613c1afb9e9a0e6c9a38fa852d16bdfd2c
yonggsnap <ronald.yonggi@snappymob.com>
2024-11-13 13:48:00 +0800

    Using Composition API

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
-<script>
-  export default {
-    data() {
-      return {
-        name: 'John Doe',
-        status: 'active',
-        tasks: ['Task One', 'Task Two', 'Task Three'],
-        link: 'https://google.com'
-      }
-    },
-    methods: {
-      toggleStatus() {
-        switch (this.status) {
-          case 'active':
-            this.status = 'pending'
-            break
-          case 'pending':
-            this.status = 'inactive'
-            break
-          default:
-            this.status = 'active'
-        }
-      }
-    }
+<script setup>
+import { ref } from "vue";
+
+const name = ref("John Doe");
+const status = ref("active");
+const tasks = ref(["Task One", "Task Two", "Task Three"]);
+const toggleStatus = () => {
+  switch (status.value) {
+    case "active":
+      status.value = "pending";
+      break;
+    case "pending":
+      status.value = "inactive";
+      break;
+    default:
+      status.value = "active";
   }
+};
 </script>
 
 <template>
@@ -35,9 +28,7 @@
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
-  <!-- <a v-bind:href="link">Click for Google</a> -->
-  <a :href="link">Click for Google</a>
-  <br/>
+  <br />
   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
   <button @click="toggleStatus">Change Status</button>
 </template>
